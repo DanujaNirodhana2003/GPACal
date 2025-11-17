ab601e2205c7aa7e8b7d2d079fce1ed985a1098b
DanujaNirodhana <chandrasenadn@gmail.com>
2025-11-17 19:46:38 +0530

    Set base path in vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/GPACal/',
   server: {
     host: "::",
     port: 8080,
