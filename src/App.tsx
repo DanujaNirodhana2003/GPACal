30936ec3fadf687dabeb470acb4fedae9bfbdd43
Danuja Nirodhana <e22054@eng.pdn.ac.lk>
2025-11-17 20:15:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/GPACal/">
           <div className="min-h-screen w-full">
             <Sidebar />
             <Routes>
